5a0aee6b28dd294500e60a8dfac858a97502d42a
Yury Butskevich <bxyura@gmail.com>
2024-04-08 23:32:02 +0300

    feat: add feedback-form to feedbacl-section

--- src/pages/PageMain.js
+++ src/pages/PageMain.js
@@ -43,8 +43,24 @@ export const PageMain = () => {
       <section className="gallery-section" id="gallery">
         <div className="wrapper">
           <h2 className="gallery-section__title">Галерея</h2>
-
           <Gallery />
+        </div>
+      </section>
+
+      <section className="feedback-section" id="feedback">
+        <div className="wrapper">
+
+          <form className="feedback-form" action="/action_page.php" method="get">
+            <h3 className="feedback-form__title">Заявка на диагностику</h3>
+            <p className="feedback-form__description">Доверьте свои медицинские исследования надежным капибарам-лаборантам и получите качественные результаты в кратчайшие сроки!</p>
+            <fieldset className="feedback-form__box-inputs">
+              <input className="feedback-form__box-inputs__input" autoComplete="off" placeholder="Фамилия" id="input-surname" pattern="^[a-zA-Zа-яА-Я-]*$" type={'text'} min={2} required></input>
+              <input className="feedback-form__box-inputs__input" autoComplete="off" placeholder="Имя" id="input-name" pattern="^[a-zA-Zа-яА-Я-]*$" type={'text'} min={2} required></input>
+              <input className="feedback-form__box-inputs__input" autoComplete="off" placeholder="Отчество" id="input-patronymic" pattern="^[a-zA-Zа-яА-Я-]*$" type={'text'} min={2} required></input>
+            </fieldset>
+            <button className="feedback-form__btn flex-center">Отправить<img src="./images/svg/check.svg" alt="Check" /></button>
+          </form>
+
         </div>
       </section>
     </main>
