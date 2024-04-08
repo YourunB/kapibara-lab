a9eea57190f59fdb0e688baf475df61704da90de
Yury Butskevich <bxyura@gmail.com>
2024-04-08 19:12:55 +0300

    feat: add section for results

--- src/pages/PageMain.js
+++ src/pages/PageMain.js
@@ -2,5 +2,22 @@ import React from "react";
 import "./PageMain.sass";
 
 export const PageMain = () => {
-  return <main className="page-main">Hello</main>;
+  return (
+    <main className="main">
+      <section className="results-section">
+        <div className="wrapper">
+
+          <div className="results-section__text">
+            <p className="results-section__text__top flex-center">ТОЧНЫЕ РЕЗУЛЬТАТЫ</p>
+            <p className="results-section__text__bottom flex-center">ТОЧНО В СРОК</p>
+          </div>
+
+          <div className="results-section__kapibara">
+            <img className="results-section__kapibara__img" src="./images/kapibara.png" alt='Kapibara' />
+          </div>
+
+        </div>
+      </section>
+    </main>
+  );
 };
